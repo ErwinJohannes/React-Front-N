5dd4748950d434ec48214c049d817b21d32cb9da
ErwinJohannes <adithep817@gmail.com>
2024-02-29 14:56:52 +0700

    mail

--- ReactFront-N/src/App.jsx
+++ ReactFront-N/src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Hello from './assets/Hello';
+import Contact from './assets/Contact';
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  console.log("App component");
+  const helloData = [
+    {name: "Anirach" , message: "Good Morning"},
+    {name: "Racha" , message: "Good Night"},
+  ];
+
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+   <div>
+    {helloData.map((data,index) => (
+      <Hello key={index} name ={data.name} message = {data.message} />
+    ))}
+
+  <Contact email="Anirach@gmail.com" phone = "02115365" />
+    </div>
+  );
 }
 
 export default App
